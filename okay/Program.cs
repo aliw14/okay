29bd4121e83672b60eacebed5f44f6525163593a
ali <ali@MacBook-Pro-Ali.local>
2023-01-03 00:49:17 +0400

    mesaj

---
+++ okay/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace okay;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        int count = 0;
+        int[] numbers = { 2, 4, 6, 5, 7, -5, -8 };
+        for (int i = 0; i < numbers.Length; i++)
+        {   if (numbers[i] > 0)
+                if (numbers[i] % 2 != 0) 
+            {
+                sum += numbers[i];
+                count++;
+
+            }
+        
+
+        }Console.WriteLine(sum);
+        Console.WriteLine(count);
+    }
+}
+
